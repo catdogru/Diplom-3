a64529c54b31318d012e48328f52cf0246ea118c
daria.i <dvigina@yandex.ru>
2024-06-14 01:59:29 +0300

    review

--- Diplom-3/src/main/java/ru/yandex/stellar/burgers/Constants.java
+++ Diplom-3/src/main/java/ru/yandex/stellar/burgers/Constants.java
@@ -1,29 +1,19 @@
 package ru.yandex.stellar.burgers;
 
-public interface Constants {
-    String STELLAR_BURGERS_BASE_API_URI = "https://stellarburgers.nomoreparties.site";
-    String API_PATH = "/api";
+public class Constants {
+    public static final String STELLAR_BURGERS_BASE_API_URI = "https://stellarburgers.nomoreparties.site";
+    public static final String API_PATH = "/api";
 
     // common json keys
-    String SUCCESS_JSON_KEY = "success";
-    String MESSAGE_JSON_KEY = "message";
+    public static final String SUCCESS_JSON_KEY = "success";
+    public static final String MESSAGE_JSON_KEY = "message";
 
-    // common messages
-    String UNAUTHORIZED_USER_MESSAGE = "You should be authorised";
-
-    interface UserConstants {
+    public static class UserConstants {
         // api path
-        String CREATE_USER_PATH = API_PATH + "/auth/register";
-        String LOGIN_USER_PATH = API_PATH + "/auth/login";
-        String UPDATE_USER_PATH = API_PATH + "/auth/user";
-        String DELETE_USER_PATH = API_PATH + "/auth/user";
+        public static final String CREATE_USER_PATH = API_PATH + "/auth/register";
+        public static final String DELETE_USER_PATH = API_PATH + "/auth/user";
 
         // response message
-        String USER_ALREADY_EXISTS_MESSAGE = "User already exists";
-        String EMPTY_REQUIRED_FIELD_MESSAGE = "Email, password and name are required fields";
-        String USER_REMOVED_MESSAGE = "User successfully removed";
-        String INCORRECT_CREDENTIALS_MESSAGE = "email or password are incorrect";
-        String EMAIL_ALREADY_IN_USE_MESSAGE = "User with such email already exists";
-
+        public static final String USER_REMOVED_MESSAGE = "User successfully removed";
     }
 }
--- Diplom-3/src/test/java/ru/yandex/stellar/burgers/constants/Constants.java
+++ Diplom-3/src/test/java/ru/yandex/stellar/burgers/constants/Constants.java
@@ -5,18 +5,18 @@ import lombok.Getter;
 import static java.util.Arrays.stream;
 import static org.apache.commons.lang3.RandomStringUtils.randomNumeric;
 
-public interface Constants {
-    String STELLAR_BURGER_BASE_SITE_URL = "https://stellarburgers.nomoreparties.site";
-    String STELLAR_BURGER_REGISTER_URL = STELLAR_BURGER_BASE_SITE_URL + "/register";
-    String STELLAR_BURGER_LOGIN_URL = STELLAR_BURGER_BASE_SITE_URL + "/login";
-    String STELLAR_BURGER_FORGOT_PASSWORD_URL = STELLAR_BURGER_BASE_SITE_URL + "/forgot-password";
-    String ACCESS_TOKEN_LOCAL_STORAGE_ITEM = "accessToken";
+public class Constants {
+    public static final String STELLAR_BURGER_BASE_SITE_URL = "https://stellarburgers.nomoreparties.site";
+    public static final String STELLAR_BURGER_REGISTER_URL = STELLAR_BURGER_BASE_SITE_URL + "/register";
+    public static final String STELLAR_BURGER_LOGIN_URL = STELLAR_BURGER_BASE_SITE_URL + "/login";
+    public static final String STELLAR_BURGER_FORGOT_PASSWORD_URL = STELLAR_BURGER_BASE_SITE_URL + "/forgot-password";
+    public static final String ACCESS_TOKEN_LOCAL_STORAGE_ITEM = "accessToken";
 
-    String CLASS_ATTRIBUTE = "class";
-    String containsCurrent = ".*current.*";
+    public static final String CLASS_ATTRIBUTE = "class";
+    public static final String containsCurrent = ".*current.*";
 
     @Getter
-    enum Browser {
+    public enum Browser {
         YANDEX_BROWSER("yandex"),
         FIREFOX_BROWSER("firefox"),
         CHROME_BROWSER("chrome"),
@@ -36,18 +36,18 @@ public interface Constants {
         }
     }
 
-    interface SystemProperties {
-        String BROWSER_PROPERTY = "browser";
-        String DRIVER_VERSION_PROPERTY = "driver.version";
-        String DRIVER_BINARY_PROPERTY = "driver.bin";
+    public static class SystemProperties {
+        public static final String BROWSER_PROPERTY = "browser";
+        public static final String DRIVER_VERSION_PROPERTY = "driver.version";
+        public static final String DRIVER_BINARY_PROPERTY = "driver.bin";
     }
 
-    interface Data {
-        String DEFAULT_USER_NAME = "Тестовый Тест Тестович";
-        String DEFAULT_PASSWORD = "Qwerty12345";
-        String SHORT_PASSWORD = "Qw..";
+    public static class Data {
+        public static final String DEFAULT_USER_NAME = "Тестовый Тест Тестович";
+        public static final String DEFAULT_PASSWORD = "Qwerty12345";
+        public static final String SHORT_PASSWORD = "Qw..";
 
-        static String generateRandomEmail() {
+        public static String generateRandomEmail() {
             return "testEmail_" + randomNumeric(4) + "@yandex.ru";
         }
     }
